bf0d7fd77e5eee2aba493f409877f302ec73fd44
Thomas Suphona <thomas.suphona@gmail.com>
2023-12-04 14:09:24 +0100

    Fully complete Day1

--- modules/day1/src/day1.cpp
+++ modules/day1/src/day1.cpp
@@ -3,19 +3,34 @@
 #include <fstream>
 #include <iostream>
 #include <list>
+#include <map>
 #include <sstream>
 #include <string>
 
-static std::list<int> GetDigitsInString(const std::string& line) {
+static std::list<int> GetDigitsInString(std::string& line) {
   int nbr_characters = line.size();
   std::list<int> digits;
-  const char* chars = line.c_str();
 
-  for (size_t i = 0; i < nbr_characters; i++) {
-    int ch_ascii = static_cast<int>(chars[i]);
-    if (ch_ascii > 47 && ch_ascii < 58) {
-      int number = ch_ascii - 48;
-      digits.push_back(number);
+  // Check spelled out digits.
+  std::list<std::string> lookup_table{"one",   "1", "two",   "2", "three", "3",
+                                      "four",  "4", "five",  "5", "six",   "6",
+                                      "seven", "7", "eight", "8", "nine",  "9"};
+
+  std::map<std::string, int> str_to_int{
+      {"one", 1},   {"1", 1}, {"two", 2},   {"2", 2}, {"three", 3}, {"3", 3},
+      {"four", 4},  {"4", 4}, {"five", 5},  {"5", 5}, {"six", 6},   {"6", 6},
+      {"seven", 7}, {"7", 7}, {"eight", 8}, {"8", 8}, {"nine", 9},  {"9", 9}};
+
+  int nbr_table_elements = lookup_table.size();
+
+  for (size_t i = 0; i < line.size(); i++) {
+    for (std::string ref : lookup_table) {
+      int l = ref.size();
+      std::string sub_string = line.substr(i, l);
+
+      if (sub_string == ref) {
+        digits.push_back(str_to_int[ref]);
+      }
     }
   }
   return digits;
--- modules/day1/tests/test_day1.cpp
+++ modules/day1/tests/test_day1.cpp
@@ -6,11 +6,23 @@
 
 #include "day1.hpp"
 
-TEST(Day1Test, BasicAssertions) {
+TEST(Day1Test, DigitsInStringAssertions) {
   std::string str = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet\n";
   std::istringstream iss(str);
 
   int sum = Day1(iss);
 
   EXPECT_EQ(sum, 142);
+}
+
+TEST(Day1Test, SpelledOutDigitsAssertions) {
+  std::string str =
+      "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\n"
+      "zoneight234\n7pqrstsixteen";
+
+  std::istringstream iss(str);
+
+  int sum = Day1(iss);
+
+  EXPECT_EQ(sum, 281);
 }
